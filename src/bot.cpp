d1a654d574bcb3fe43b6b23f9a473e917a1f8efe
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-09-30 18:40:08 +0200

    Verify config file path

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,5 +1,8 @@
 #include <build-bot/bot.h>
 
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
 using namespace dsn::build_bot;
 
 namespace dsn {
@@ -10,6 +13,18 @@ namespace build_bot {
             bool loadConfig(const std::string& config_file)
             {
                 BOOST_LOG_SEV(log, severity::info) << "Loading configuration from " << config_file;
+
+                fs::path path(config_file);
+                if (!fs::exists(path)) {
+                    BOOST_LOG_SEV(log, severity::error) << "Config file " << config_file << " doesn't exist!";
+                    return false;
+                }
+
+                if (!fs::is_regular_file(path)) {
+                    BOOST_LOG_SEV(log, severity::error) << "Config file " << config_file << " isn't a regular file!";
+                    return false;
+                }
+
                 return true;
             }
 
