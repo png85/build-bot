8e2d62ae4c90b4a9e6d0795158d7e00303997ffe
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-09-30 18:13:25 +0200

    Add command line parsing to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
-int main(int argc, char** argv) {
-  return 0;
+#include <boost/log/trivial.hpp>
+#include <boost/program_options.hpp>
+
+#include <dsnutil/log/init.h>
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv)
+{
+    dsn::log::init();
+
+    try {
+        po::options_description descr;
+        descr.add_options()("help,?", "Display list of valid arguments");
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, descr), vm);
+        po::notify(vm);
+    }
+
+    catch (po::error& ex) {
+        BOOST_LOG_TRIVIAL(error) << "Failed to parse command line arguments: " << ex.what();
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
