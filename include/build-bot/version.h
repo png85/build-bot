b13ff48a49e386c538ab8681463d04eff49ee095
Peter Hille (png!das-system) <peter@das-system-networks.de>
2015-09-30 17:59:02 +0200

    Compile auto-generated version info file

--- include/build-bot/version.h
+++ include/build-bot/version.h
@@ -2,15 +2,17 @@
 #ifndef BUILD_BOT_VERSION_H
 #define BUILD_BOT_VERSION_H
 
+#include <cstdint>
+
 namespace dsn {
 namespace build_bot {
     namespace version {
-        extern static const uint32_t MAJOR;
-        extern static const uint32_t MINOR;
-        extern static const uint32_t PATCH;
+        extern const uint32_t MAJOR;
+        extern const uint32_t MINOR;
+        extern const uint32_t PATCH;
 
-        extern static const char GIT_SHA1[];
-        extern static const char GIT_REFSPEC[];
+        extern const char GIT_SHA1[];
+        extern const char GIT_REFSPEC[];
     }
 }
 }
